20c0fe4d9dd4060ca23058d921abc5f0f0bd1e20
Deepender <deepender07@gmail.com>
2020-05-27 00:14:43 +0530

    Add restricted auto mod

--- app.js
+++ app.js
@@ -37,7 +37,66 @@ bot.on("disconnected", function () {
     }, 2000);
 });
 
-  // also, reconnect the bot hourly.
+bot.on('message', function(message) {
+    const regex = /buyer/img;
+    const str = message.content;
+    let m;
+
+    while ((m = regex.exec(str)) !== null) {
+        if (m.index === regex.lastIndex) {
+            regex.lastIndex++;
+        }
+        var r = m.some(function(match, groupIndex) {
+            //console.log(`Found slag key match, group ${groupIndex}: ${match}`);
+            //console.log(`message.channel.id => ${message.channel.id}`);
+            if(match == 'buyer' && message.channel.id == config.general_chat_id) {
+                if (message) message.delete();
+                const embed = new Discord.RichEmbed()  
+                .setAuthor(`${message.author.username} has been warned`, message.author.avatarURL)
+                .setDescription('**Reason:** You are not allowed to sell anything in the server.')
+                .setTimestamp()
+                .setFooter('From DarkTangent Team', 'https://www.risingcup.com/assets/images/dt_logo.png');
+                message.channel.send({ embed: embed });
+                return true;
+            }
+        });
+        if(r) break;
+    }
+
+    //Restricted Keywords
+    let keywords = ['id','pass','need','players'];
+    for (let index = 0; index < keywords.length; index++) {
+        const element = new RegExp(keywords[index],'img');
+        let closeLoop = false;
+        while ((m = element.exec(str)) !== null) {
+            if (m.index === element.lastIndex) {
+                element.lastIndex++;
+            }
+            var r = m.some(function(match, groupIndex) {
+                //console.log(`Found slag key match, group ${groupIndex}: ${match}`);
+                if(message.channel.id == config.general_chat_id) 
+                {
+                    if (message) message.delete();
+                    const embed = new Discord.RichEmbed()  
+                    .setAuthor(`${message.author.username} has been warned`, message.author.avatarURL)
+                    .setDescription(`**Reason:** Keywords such as **id**, **pass**, **need**, **player** is restricted in public_chat, post such message in  ☛  <#650316706194915370>`)
+                    .setTimestamp()
+                    .setFooter('From DarkTangent Team', 'https://www.risingcup.com/assets/images/dt_logo.png');
+                    message.channel.send({ embed: embed });
+                    return true;
+                }
+            });
+            if(r) {
+                closeLoop = r;
+                break;
+            }
+        }
+        if(closeLoop)
+            break;
+    }
+});
+
+// also, reconnect the bot hourly.
 setInterval(function() {
     console.log(`Reconnecting to server per timeout of ${config.reconnect} ms.`);
     bot.destroy();
